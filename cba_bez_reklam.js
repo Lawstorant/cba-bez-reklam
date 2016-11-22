a63f033d3a65b8ae78b11f47fc577deff081a837
Lawstorant <forest10pl@gmail.com>
2016-11-22 19:01:05 +0100

    poprawiona wersja

--- cba_bez_reklam.js
+++ cba_bez_reklam.js
@@ -4,23 +4,24 @@ Możliwe że będzie działać na innych hostingach.
 Warunkiem jest używanie znaczników <div> lub
 <iframe> przez hosting.
 
-Pamiętaj dodać te znaczniki.
+Pamiętaj by skrypt umieścić na końcu strony
+i aby dodać te znacznik:
 
 <div id="stop_reklam"></div>
-<kod strony>
+<kod strony>P
 <div id="start_reklam"></div>
 
 Pokazują one skryptowi gdzie znajduje
 się kod strony.*/
 
-window.onload = function(){
-	a = document.getElementsByTagName("div");
-	b = document.getElementsByTagName("iframe");
-	f1 = true;
-	for(i = 0; i < a.length; ++i){
-		if(a[i].id == "start_reklam") f1 = true;
-		if(f1) a[i].style.display = "none";
-		if(a[i].id == "stop_reklam") f1 = false;
-		b[i].style.display = "none";
-	}
-};
+a = document.getElementsByTagName("div");
+b = document.getElementsByTagName("iframe");
+var f1 = true;
+for(i = 0; i < a.length; ++i){
+	if(a[i].id == "start_reklam") f1 = true;
+	if(f1) a[i].style.display = "none";
+	if(a[i].id == "stop_reklam") f1 = false;
+}
+for(i = 0; i < b.length; ++i){
+	b[i].style.display = "none";
+}

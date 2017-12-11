e49b95c7185779b363ea5c18b585f4d6c353820b
Lawstorant <forest10pl@gmail.com>
2017-12-11 21:29:50 +0100

    Zaktualizowano skrypt do ES6 i poprawiono ładowanie. Teraz można dodać go w <head>.

--- cba_bez_reklam.js
+++ cba_bez_reklam.js
@@ -1,11 +1,13 @@
-a = document.getElementsByTagName("div");
-b = document.getElementsByTagName("iframe");
-var f1 = true;
-for(i = 0; i < a.length; ++i){
-	if(a[i].id == "start_reklam") f1 = true;
-	if(f1) a[i].style.display = "none";
-	if(a[i].id == "stop_reklam") f1 = false;
-}
-for(i = 0; i < b.length; ++i){
-	b[i].style.display = "none";
-}
+document.addEventListener("DOMContentLoaded", function(){
+	let a = document.getElementsByTagName("div");
+	let b = document.getElementsByTagName("iframe");
+	let  f1 = true;
+	for(let i = 0; i < a.length; ++i){
+		if(a[i].id == "start_reklam") f1 = true;
+		if(f1) a[i].style.display = "none";
+		if(a[i].id == "stop_reklam") f1 = false;
+	}
+	for(let i = 0; i < b.length; ++i){
+		b[i].style.display = "none";
+	}
+});

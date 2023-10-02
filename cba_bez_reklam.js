6a30fe9c4c9e89aee7a5854dc731cb48e8c8e54e
Tomasz Pakuła <tomasz.pakula@sysadmins.pl>
2023-10-02 13:38:54 +0200

    XDe

--- cba_bez_reklam.js
+++ cba_bez_reklam.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 	let a = document.getElementsByTagName("div");
 	let b = document.getElementsByTagName("iframe");
 	let  f1 = true;
+	//qwe
 	for(let i = 0; i < a.length; ++i){
 		if(a[i].id == "start_reklam") f1 = true;
 		if(f1) a[i].style.display = "none";
